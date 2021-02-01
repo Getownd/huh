a06c9054c06692b615689364577199f6b2968710
developer <developer@SERGNOTEBOOK>
2021-02-01 14:37:20 +0500

    first

---
+++ pro100Server/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pro100Server.Startup))]
+namespace pro100Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
